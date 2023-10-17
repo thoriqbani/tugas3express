4b7aa5481d3d9f9ae1e82cfe3ec2b7c3fc3c5e3c
thoriq bani <tjrsgamers@gmail.com>
2023-10-17 15:23:15 +0700

    baru lagi

---
+++ frontend/src/pages/Mahasiswa.js
@@ -0,0 +1,15 @@
+import {Container, Row, Col } from 'react-bootstrap';
+
+function Mahasiswa(){
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h2>Data Mahasiswa</h2>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Mahasiswa;
